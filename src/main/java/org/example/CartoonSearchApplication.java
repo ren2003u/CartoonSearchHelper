9070b73c8210c75050118bdac9a81027e9211a1b
jiahao ren <r542251108@gmail.com>
2023-08-05 21:32:45 +0800

    resolve CORS issue by adding WebMvcConfigurer function in CartoonSearchApplication.

--- src/main/java/org/example/CartoonSearchApplication.java
+++ src/main/java/org/example/CartoonSearchApplication.java
@@ -2,11 +2,28 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CartoonSearchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CartoonSearchApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3001") // React app's address
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
